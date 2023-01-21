d43719f1098cd480da3f1ba12bcc4f55152dd46d
Abhishek <abhisheksinghasr734@gmail.com>
2023-01-21 23:59:21 +0530

    practice

---
+++ practice/index.js
@@ -0,0 +1,66 @@
+// for leap year
+
+// function leapyear(year){
+//     return ((year%4==0)&&(year%100!==0)||(year%400==0))
+// }
+
+// console.log(leapyear(2000))
+
+
+// let num = 5;
+// let i =1 ;
+
+// console.log("Multiplication table of "+num);
+
+// while(i<=20){
+//     console.log(`${num} X ${i} = ${num*i}`);
+//     i++;
+// }
+
+
+
+// program to reverse a number
+
+// let num="12";
+// let newnum="";
+// while(num>0){
+//     newnum=newnum+num%10;
+//     num=Math.floor(num/10)
+// }
+// console.log(newnum)
+
+
+
+// let image = document.getElementById("image");
+
+// image.addEventListener("mouseover",function(){
+//     image.style.transform="scale(1.5)";
+// })
+
+// image.addEventListener("mouseout", function(){
+//     image.style.transform="scale(1)"
+// })
+
+
+// function celToFar(c){
+//     console.log(c*9/5+32+"in farhenhite");
+// }
+// let cel=-40;
+// celToFar(cel)
+
+// function isPlaindrome(num){
+//     let reversenum = num.toString().split("").reverse().join("");
+//     return num == reversenum;
+
+// }
+// console.log(isPlaindrome(12343212));
+
+// let age = prompt("Enter your age");
+
+// let birthdayMessage = "";
+// for (let i = 0; i < age; i++) {
+//     birthdayMessage += `<p>${i+1}happy birthday</p>`;
+// }
+
+// document.body.innerHTML=birthdayMessage;
+
